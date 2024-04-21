0de1c1ef1c1ba9313342239260b915349bfc0d52
ИГОРЬ ЭТО РАЙФАЙЗЕН БАНК <150173920+DILDOvzlom@users.noreply.github.com>
2024-04-21 20:06:14 +0300

    Add files via upload

---
+++ ConsoleApp1/Students.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public  class Students
+    {
+       
+        private string StudentNumber;
+        private string StudentName;
+       
+
+        private Students()
+        {
+
+        }
+        public Students(string studentnumber,string studentname)
+        {
+            StudentNumber = studentnumber;
+            StudentName = studentname;
+          
+        }
+        public string Student_Number
+        {
+            get {return StudentNumber; }
+        }
+        public string Student_Name
+        {
+            get { return StudentName; }
+        }
+       
+
+        public string PassType(double average)
+        {
+            string Start = StudentName + "(" + StudentNumber + ")";
+            if (average>=75 && average<=100)
+            {
+                return Start+":Distinction";
+            }
+            else if(average >=50 && average <75)
+            {
+                return Start + ":Pass";
+            }
+            else if(average>=0 && average <50)
+            {
+                return Start + ":Fail";
+            }
+            else
+            {
+                return Start + ":Inccorect Average";
+            }
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StudentTypePassWithDistinction()
+        {
+       
+            Students students = new Students("21612345","Todd"); 
+            string expected =students.Student_Name+"("+students.Student_Number+")"+ ":Distinction";
+            string actual = students.PassType(average); 
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void StudentTypePass()
+        {
+    
+            Students students = new Students("2161433","Bill"); 
+            double average = 49;
+            string expected = students.Student_Name + "(" + students.Student_Number + ")" + ":Pass";
+            string actual = students.PassType(average); 
+            //Assert
+            Assert.AreEqual(expected, actual);
+        
+
+    }
+
+        [TestMethod]
+        public void StudentTypeFail()
+        {
+
+        }
+
+        [TestMethod]
+        public void StudentTypeIncorrectAverageGreaterThan100()
+        {
+
+
+        }
+       
+        [TestMethod]
+        public void ChangeName()
+        {
+
+
+        }
+
+    }
+}
